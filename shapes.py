92c46e997ac3cf336ad18d17e22ccbdf64f31193
Kabir Singh <kabirsingh5102@gmail.com>
2025-09-07 12:19:39 +0530

    s

--- shapes.py
+++ shapes.py
@@ -1,7 +1,7 @@
 """
 Inheritance Practice
 """
-
+import numpy as np
 class Shape:
     def __init__(self , color = "black"):
         self._color = color
@@ -16,4 +16,29 @@ class Rectangle(Shape):
         self.l = sidea
         self.b = sideb
         super().__init__(color=color)
-        
+    def area(self):
+        return self.l*self.b
+
+class Triangle(Shape):
+    def __init__(self , base , height , color = "black"):
+        self.b = base
+        self.h = height
+        super().__init__(color=color)
+    def area(self):
+        return 0.5*self.l*self.b
+
+class Ellipse(Shape):
+    def __init__(self , major_r  ,  minor_r,color = "black"):
+        self.major_r = major_r
+        self.minor_r = minor_r
+        super().__init__(color=color)
+    def area(self):
+        return np.pi*self.major_r*self.minor_r
+
+def turnred(shapeobject):
+    if isinstance(shapeobject , Shape):
+        shapeobject._color = "Red"
+        return shapeobject
+    else:
+        raise TypeError("input needs to be an instance of Class Shape")
+    
